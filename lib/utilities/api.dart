8a00f9f9410676d3c6072f8ed3276d3addfaeda9
Atanu Debnath <playatanuyt@gmail.com>
2021-10-18 17:21:21 +0530

    api update

--- lib/utilities/api.dart
+++ lib/utilities/api.dart
@@ -2,17 +2,18 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-const String api = '898A4F13-6C22-48B9-8D8D-230DE095A72D';
+const String apikey = '898A4F13-6C22-48B9-8D8D-230DE095A72D';
 
 class ApiService {
-  Future<List> getPost(_chosenValue) async {
+  Future<List> getPost() async {
     try {
       String url =
-          '//https://rest.coinapi.io/v1/exchangerate/BTC/$_chosenValue?apikey=$api';
+          'https://rest.coinapi.io/v1/assets/?apikey=$apikey';
+
       var response = await http.get(Uri.parse(url));
+      // ignore: avoid_print
       print(response.body);
       return jsonDecode(response.body);
-      print(response.body);
     } catch (e) {
       return Future.error('error');
     }
